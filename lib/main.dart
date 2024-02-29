49fb5e37dc5af4242ecc6c81afae1d6f4a0cf4cf
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-29 13:10:47 +0530

    new contract

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'contract_linking.dart';
+import 'helloUI.dart';
 
 void main() {
   runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          body: Column(
-        children: [
-          Text('hi'),
-        ],
-      )),
+    // Inserting Provider as a parent of HelloUI()
+    return ChangeNotifierProvider<ContractLinking>(
+      create: (_) => ContractLinking(),
+      child: MaterialApp(
+        title: "Hello World",
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.cyan[400],
+            hintColor: Colors.deepOrange[200]),
+        home: HelloUI(),
+      ),
     );
   }
 }
